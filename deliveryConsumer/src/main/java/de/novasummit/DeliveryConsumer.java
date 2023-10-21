8a0840c74a9ee9731059221de30e46d146c3bb08
dri <dri@novatec-gmbh.de>
2023-10-21 17:28:12 +0200

    add Spring main classes

---
+++ deliveryConsumer/src/main/java/de/novasummit/DeliveryConsumer.java
@@ -0,0 +1,11 @@
+package de.novasummit;
+
+import org.springframework.boot.SpringApplication;
+
+public class DeliveryConsumer {
+    public static void main(String[] args) {
+
+        SpringApplication.run(DeliveryConsumer.class, args);
+
+    }
+}
---
+++ ordersProducer/src/main/java/de/novasummit/OrderProducer.java
@@ -0,0 +1,13 @@
+package de.novasummit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProducer {
+    public static void main(String[] args) {
+
+        SpringApplication.run(OrderProducer.class, args);
+
+    }
+}
