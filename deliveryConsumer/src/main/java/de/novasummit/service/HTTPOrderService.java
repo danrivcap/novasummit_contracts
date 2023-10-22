ee5a88f13250ccc50d1367d4a10b08dfb6891cb4
dri <dri@novatec-gmbh.de>
2023-10-22 16:32:49 +0200

    Order web client

--- deliveryConsumer/src/main/java/de/novasummit/service/HTTPOrderService.java
+++ deliveryConsumer/src/main/java/de/novasummit/service/HTTPOrderService.java
@@ -2,11 +2,21 @@ package de.novasummit.service;
 
 import de.novasummit.domain.OrderResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
 
 @Service
 public class HTTPOrderService implements OrderService {
+
     @Override
     public OrderResponse getOrder(int id) {
-        return null;
+
+        WebClient client = WebClient.builder().baseUrl("http://localhost:8080").build();
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+
+        OrderHttpClient orderClient = factory.createClient(OrderHttpClient.class);
+        return orderClient.getOrder(id + "");
     }
 }
---
+++ deliveryConsumer/src/main/java/de/novasummit/service/OrderHttpClient.java
@@ -0,0 +1,14 @@
+package de.novasummit.service;
+
+import de.novasummit.domain.OrderResponse;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+
+public interface OrderHttpClient {
+
+    @GetExchange("/order/{id}")
+    OrderResponse getOrder(@PathVariable("id") String id);
+
+}
