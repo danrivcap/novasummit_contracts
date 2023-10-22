eed118d9866229ad3c3308ffa2ffbcfa5595010a
dri <dri@novatec-gmbh.de>
2023-10-22 11:32:54 +0200

    Fix some DI problems

--- deliveryConsumer/src/main/java/de/novasummit/DeliveryConsumer.java
+++ deliveryConsumer/src/main/java/de/novasummit/DeliveryConsumer.java
@@ -1,7 +1,9 @@
 package de.novasummit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class DeliveryConsumer {
     public static void main(String[] args) {
 
--- deliveryConsumer/src/main/java/de/novasummit/service/HTTPOrderService.java
+++ deliveryConsumer/src/main/java/de/novasummit/service/HTTPOrderService.java
@@ -1,7 +1,9 @@
 package de.novasummit.service;
 
 import de.novasummit.domain.OrderResponse;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HTTPOrderService implements OrderService {
     @Override
     public OrderResponse getOrder(int id) {
